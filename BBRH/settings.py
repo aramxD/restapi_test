4acd4dd2f8f839e7e39ea716f120ade78f309fa3
aramxD <35883004+aramxD@users.noreply.github.com>
2022-09-01 17:25:18 -0700

    CORS FIXED

--- BBRH/settings.py
+++ BBRH/settings.py
@@ -173,6 +173,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://localhost:8000',
     'http://127.0.0.1:5500',
-    'https://juliosantacruz.dev'
+    'https://juliosantacruz.dev',
+    'https://www.quimikal.com',
      
     )
